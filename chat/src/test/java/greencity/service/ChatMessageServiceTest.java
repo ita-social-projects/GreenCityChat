1dff7d500960cff04067f03d0119ecf01953c433
Woland <gutejvova@gmail.com>
2021-03-22 16:57:00 +0200

    tests

--- chat/src/test/java/greencity/service/ChatMessageServiceTest.java
+++ chat/src/test/java/greencity/service/ChatMessageServiceTest.java
@@ -1,18 +1,22 @@
 package greencity.service;
 
+import greencity.constant.ErrorMessage;
 import greencity.dto.ChatMessageDto;
 import greencity.dto.ChatRoomDto;
+import greencity.dto.MessageLike;
 import greencity.entity.ChatMessage;
 import greencity.entity.ChatRoom;
 import greencity.entity.Participant;
+import greencity.exception.exceptions.ChatRoomNotFoundException;
 import greencity.repository.ChatMessageRepo;
 import greencity.repository.ChatRoomRepo;
 import greencity.service.impl.ChatMessageServiceImpl;
-import java.util.Collections;
-import java.util.Optional;
+import java.time.ZonedDateTime;
+import java.util.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -20,9 +24,8 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -53,6 +56,16 @@ class ChatMessageServiceTest {
             .roomId(1L)
             .senderId(1L)
             .content("test")
+            .build();
+
+        expectedChatMessage = ChatMessage.builder()
+            .id(1L)
+            .content("content")
+            .sender(new Participant())
+            .room(new ChatRoom())
+            .createDate(ZonedDateTime.now())
+            .fileName("filename")
+            .fileType("fileType")
             .build();
     }
 
@@ -93,11 +106,88 @@ class ChatMessageServiceTest {
     }
 
     @Test
+    public void findAllMessagesByChatRoomIdFailed() {
+        when(chatRoomRepo.findById(anyLong()))
+            .thenThrow(new ChatRoomNotFoundException(ErrorMessage.CHAT_ROOM_NOT_FOUND_BY_ID));
+
+        assertThrows(ChatRoomNotFoundException.class,
+            () -> chatMessageServiceImpl.findAllMessagesByChatRoomId(1L));
+    }
+
+    @Test
     void processMessage() {
         when(modelMapper.map(expectedChatMessageDto, ChatMessage.class)).thenReturn(expectedChatMessage);
         when(chatMessageRepo.save(expectedChatMessage)).thenReturn(expectedChatMessage);
         chatMessageServiceImpl.processMessage(expectedChatMessageDto);
         verify(messagingTemplate).convertAndSend("/room/" + expectedChatMessageDto.getRoomId() + "/queue/messages",
             expectedChatMessageDto);
+    }
+
+    @Test
+    public void deleteMessage() {
+        when(modelMapper.map(expectedChatMessageDto, ChatMessage.class)).thenReturn(expectedChatMessage);
+
+        chatMessageServiceImpl.deleteMessage(expectedChatMessageDto);
+
+        verify(chatMessageRepo).delete(expectedChatMessage);
+        verify(messagingTemplate).convertAndSend(any(String.class),
+            any(ChatMessageDto.class), any(Map.class));
+    }
+
+    @Test
+    public void updateMessage() {
+        expectedChatMessageDto.setId(1L);
+        when(modelMapper.map(expectedChatMessageDto, ChatMessage.class)).thenReturn(expectedChatMessage);
+        when(chatMessageRepo.getLikesByMessageId(anyLong()))
+            .thenReturn(Collections.singletonList(expectedChatMessageDto.getId()));
+        when(chatMessageRepo.save(expectedChatMessage)).thenReturn(expectedChatMessage);
+
+        chatMessageServiceImpl.updateMessage(expectedChatMessageDto);
+
+        verify(messagingTemplate).convertAndSend(any(String.class),
+            any(ChatMessageDto.class), any(Map.class));
+    }
+
+    @Test
+    public void findTopByOrderByIdDesc() {
+        when(chatMessageRepo.findTopByOrderByIdDesc()).thenReturn(expectedChatMessage);
+        when(modelMapper.map(expectedChatMessage, ChatMessageDto.class)).thenReturn(expectedChatMessageDto);
+
+        ChatMessageDto actual = chatMessageServiceImpl.findTopByOrderByIdDesc();
+
+        assertEquals(expectedChatMessageDto, actual);
+    }
+
+    @Test
+    public void likeMessage() {
+        MessageLike liked = new MessageLike(1L, 1L);
+
+        when(chatMessageRepo.getParticipantIdIfLiked(liked.getMessageId(), liked.getParticipantId())).thenReturn(1L);
+        when(chatMessageRepo.findById(liked.getMessageId())).thenReturn(Optional.of(expectedChatMessage));
+        when(modelMapper.map(any(ChatMessage.class), eq(ChatMessageDto.class))).thenReturn(expectedChatMessageDto);
+
+        chatMessageServiceImpl.likeMessage(liked);
+
+        verify(chatMessageRepo).getLikesByMessageId(liked.getMessageId());
+        verify(chatMessageRepo).deleteLikeFromMessage(liked.getMessageId(), liked.getParticipantId());
+        verify(messagingTemplate).convertAndSend(any(String.class),
+            any(ChatMessageDto.class), any(Map.class));
+    }
+
+    @Test
+    public void likeMessageUnliked() {
+        Long id = null;
+        MessageLike liked = new MessageLike(1L, 1L);
+
+        when(chatMessageRepo.getParticipantIdIfLiked(anyLong(), anyLong())).thenReturn(id);
+        when(chatMessageRepo.findById(liked.getMessageId())).thenReturn(Optional.of(expectedChatMessage));
+        when(modelMapper.map(any(ChatMessage.class), eq(ChatMessageDto.class))).thenReturn(expectedChatMessageDto);
+
+        chatMessageServiceImpl.likeMessage(liked);
+
+        verify(chatMessageRepo).addLikeToMessage(liked.getMessageId(), liked.getParticipantId());
+        verify(chatMessageRepo).getLikesByMessageId(liked.getMessageId());
+        verify(messagingTemplate).convertAndSend(any(String.class),
+            any(ChatMessageDto.class), any(Map.class));
     }
 }
