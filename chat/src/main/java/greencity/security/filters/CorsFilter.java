851bbe69d2d308209850f67a6d77e9750e7b200f
Maksym M.B. Bokhonko <maksym.bokhonko@devcom.com>
2022-05-05 00:51:15 +0300

    final cors fix

--- chat/src/main/java/greencity/security/filters/CorsFilter.java
+++ chat/src/main/java/greencity/security/filters/CorsFilter.java
@@ -7,17 +7,20 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
 public class CorsFilter extends OncePerRequestFilter {
+    @Value("${request.origin}")
+    private String requestOrigin;
     @Override
     protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response,
         final FilterChain filterChain) throws ServletException, IOException {
-        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Origin", requestOrigin);
         response.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, HEAD");
-        response.addHeader("Access-Control-Allow-Headers", "*");
+        response.addHeader("Access-Control-Allow-Headers", "X-Requested-With, Origin, Content-Type, Accept, Authorization, Access-Control-Allow-Origin");
         response.addHeader("Access-Control-Allow-Credentials", "true");
         response.addIntHeader("Access-Control-Max-Age", 10);
         filterChain.doFilter(request, response);
