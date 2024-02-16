089dc90f6a4068aeb0b2cc3a229d8a6bffcc9f39
2or5 <nazarbokalo0@gmail.com>
2024-02-16 16:51:47 +0200

    javax to jakarta

--- chat/src/main/java/greencity/security/service/TokenService.java
+++ chat/src/main/java/greencity/security/service/TokenService.java
@@ -7,7 +7,7 @@ public interface TokenService {
      * Method that pass the token to cookies.
      *
      * @param accessToken {@link String}
-     * @param response    {@link javax.servlet.http.HttpServletResponse}
+     * @param response    {@link jakarta.servlet.http.HttpServletResponse}
      */
     void passTokenToCookies(String accessToken, HttpServletResponse response);
 }
