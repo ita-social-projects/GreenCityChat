25b5e092e80c746cf0e914be511bafa9016c6ee0
2or5 <nazarbokalo0@gmail.com>
2024-03-07 15:49:25 +0200

    use plugins2

--- chat/src/main/java/greencity/security/interceptor/UserActivityInterceptor.java
+++ chat/src/main/java/greencity/security/interceptor/UserActivityInterceptor.java
@@ -1,8 +1,10 @@
 package greencity.security.interceptor;
 
+import greencity.client.RestClient;
 import lombok.AllArgsConstructor;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @AllArgsConstructor
 public class UserActivityInterceptor implements HandlerInterceptor {
+    private final RestClient restClient;
 }
