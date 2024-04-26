ceecab5f619964c9faa82b26c2547010d4471656
lubchenkodenys <denyslubchenko@gmail.com>
2024-04-26 16:14:51 +0300

    delete all wildcard imports

--- chat/src/test/java/greencity/controller/ChatControllerTest.java
+++ chat/src/test/java/greencity/controller/ChatControllerTest.java
@@ -1,7 +1,12 @@
 package greencity.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import greencity.dto.*;
+import greencity.dto.ChatMessageDto;
+import greencity.dto.ChatRoomDto;
+import greencity.dto.FriendsChatDto;
+import greencity.dto.GroupChatRoomCreateDto;
+import greencity.dto.MessageLike;
+import greencity.dto.ParticipantDto;
 import greencity.entity.Participant;
 import greencity.enums.ChatType;
 import greencity.service.AzureFileService;
@@ -17,6 +22,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
@@ -27,8 +33,10 @@ import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
@@ -66,23 +74,23 @@ class ChatControllerTest {
 
     @Test
     void findAllRoomsTest() throws Exception {
-        when(principal.getName()).thenReturn("test");
+        Mockito.when(principal.getName()).thenReturn("test");
         List<ChatRoomDto> list = new ArrayList<>();
-        when(chatRoomService.findAllByParticipantName("test")).thenReturn(list);
+        Mockito.when(chatRoomService.findAllByParticipantName("test")).thenReturn(list);
         mockMvc.perform(get(chatLink + "/").principal(principal)).andExpect(status().isOk());
 
-        verify(chatRoomService).findAllByParticipantName("test");
+        Mockito.verify(chatRoomService).findAllByParticipantName("test");
 
     }
 
     @Test
     void findAllVisibleRoomsTest() throws Exception {
-        when(principal.getName()).thenReturn("test");
+        Mockito.when(principal.getName()).thenReturn("test");
         List<ChatRoomDto> list = new ArrayList<>();
-        when(chatRoomService.findAllVisibleRooms("test")).thenReturn(list);
+        Mockito.when(chatRoomService.findAllVisibleRooms("test")).thenReturn(list);
         mockMvc.perform(get(chatLink + "/rooms/visible").principal(principal)).andExpect(status().isOk());
 
-        verify(chatRoomService).findAllVisibleRooms("test");
+        Mockito.verify(chatRoomService).findAllVisibleRooms("test");
     }
 
     @Test
@@ -90,24 +98,24 @@ class ChatControllerTest {
         mockMvc.perform(get(chatLink + "/messages/{room_id}", 1))
             .andExpect(status().isOk());
 
-        verify(chatMessageService).findAllMessagesByChatRoomId(1L, PageRequest.of(0, 20));
+        Mockito.verify(chatMessageService).findAllMessagesByChatRoomId(1L, PageRequest.of(0, 20));
     }
 
     @Test
     void findPrivateRoomWithUserTest() throws Exception {
-        when(principal.getName()).thenReturn("name");
+        Mockito.when(principal.getName()).thenReturn("name");
         mockMvc.perform(get(chatLink + "/user/{id}", 1)
             .principal(principal))
             .andExpect(status().isOk());
 
-        verify(chatRoomService).findPrivateByParticipants(1L, "name");
+        Mockito.verify(chatRoomService).findPrivateByParticipants(1L, "name");
     }
 
     @Test
     void findRoomByIdTest() throws Exception {
         mockMvc.perform(get(chatLink + "/room/{room_id}", 1))
             .andExpect(status().isOk());
-        verify(chatRoomService).findChatRoomById(1L);
+        Mockito.verify(chatRoomService).findChatRoomById(1L);
     }
 
     @Test
@@ -115,92 +123,92 @@ class ChatControllerTest {
         mockMvc.perform(delete(chatLink + "/room/{user_id}/{room_id}", 1, 1))
             .andExpect(status().isOk());
 
-        verify(chatMessageService).cleanUnreadMessages(1L, 1L);
+        Mockito.verify(chatMessageService).cleanUnreadMessages(1L, 1L);
     }
 
     @Test
     void getCurrentUserTest() throws Exception {
-        when(principal.getName()).thenReturn("testmail@gmail.com");
+        Mockito.when(principal.getName()).thenReturn("testmail@gmail.com");
         mockMvc.perform(get(chatLink + "/user")
             .principal(principal)).andExpect(status().isOk());
 
-        verify(participantService).getCurrentParticipantByEmail("testmail@gmail.com");
+        Mockito.verify(participantService).getCurrentParticipantByEmail("testmail@gmail.com");
     }
 
     @Test
     void getAllParticipantsByTest() throws Exception {
         String query = "testQuery";
-        when(principal.getName()).thenReturn("testmail@gmail.com");
+        Mockito.when(principal.getName()).thenReturn("testmail@gmail.com");
         List<ParticipantDto> listOfAllParticipantsByQuery = new ArrayList<>();
-        when(participantService.findAllParticipantsByQuery(query, "testmail@gmail.com"))
+        Mockito.when(participantService.findAllParticipantsByQuery(query, "testmail@gmail.com"))
             .thenReturn(listOfAllParticipantsByQuery);
 
         mockMvc.perform(get(chatLink + "/users/{query}", query)
             .principal(principal)).andExpect(status().isOk());
 
-        verify(participantService).findAllParticipantsByQuery(query, "testmail@gmail.com");
+        Mockito.verify(participantService).findAllParticipantsByQuery(query, "testmail@gmail.com");
     }
 
     @Test
     void getAllParticipantsByWithEmptyPrincipalTest() throws Exception {
         List<ParticipantDto> list =
             List.of(ParticipantDto.builder().id(1L).build(), ParticipantDto.builder().id(2L).build());
-        when(principal.getName()).thenReturn("testmail@gmail.com");
-        when(participantService.findAllExceptCurrentUser("testmail@gmail.com")).thenReturn(list);
+        Mockito.when(principal.getName()).thenReturn("testmail@gmail.com");
+        Mockito.when(participantService.findAllExceptCurrentUser("testmail@gmail.com")).thenReturn(list);
 
         mockMvc.perform(get(chatLink + "/users/{query}", "")
             .principal(principal)).andExpect(status().isOk());
 
-        verify(participantService).findAllExceptCurrentUser("testmail@gmail.com");
+        Mockito.verify(participantService).findAllExceptCurrentUser("testmail@gmail.com");
     }
 
     @Test
     void getAllChatRoomsByTest() throws Exception {
         String query = "testQuery";
-        when(principal.getName()).thenReturn("testmail@gmail.com");
+        Mockito.when(principal.getName()).thenReturn("testmail@gmail.com");
         Participant participant = new Participant();
-        when(participantService.findByEmail("testmail@gmail.com")).thenReturn(participant);
+        Mockito.when(participantService.findByEmail("testmail@gmail.com")).thenReturn(participant);
         List<ChatRoomDto> listOfAllChatRoomsByQuery = new ArrayList<>();
-        when(chatRoomService.findAllChatRoomsByQuery(query, participant)).thenReturn(listOfAllChatRoomsByQuery);
+        Mockito.when(chatRoomService.findAllChatRoomsByQuery(query, participant)).thenReturn(listOfAllChatRoomsByQuery);
 
         mockMvc.perform(get(chatLink + "/rooms/{query}", query)
             .principal(principal)).andExpect(status().isOk());
 
-        verify(chatRoomService).findAllChatRoomsByQuery(query, participant);
+        Mockito.verify(chatRoomService).findAllChatRoomsByQuery(query, participant);
     }
 
     @Test
     void getAllChatRoomsByWithEmptyQueryTest() throws Exception {
-        when(principal.getName()).thenReturn("test");
+        Mockito.when(principal.getName()).thenReturn("test");
         List<ChatRoomDto> list = List.of(ChatRoomDto.builder().id(1L).build());
-        when(chatRoomService.findAllVisibleRooms("test")).thenReturn(list);
+        Mockito.when(chatRoomService.findAllVisibleRooms("test")).thenReturn(list);
 
         mockMvc.perform(get(chatLink + "/rooms").principal(principal)).andExpect(status().isOk());
 
-        verify(chatRoomService).findAllVisibleRooms("test");
+        Mockito.verify(chatRoomService).findAllVisibleRooms("test");
     }
 
     @Test
     void getLastIdTest() throws Exception {
         ChatMessageDto chatMessage = new ChatMessageDto();
         chatMessage.setId(1L);
-        when(chatMessageService.findTopByOrderByIdDesc()).thenReturn(chatMessage);
+        Mockito.when(chatMessageService.findTopByOrderByIdDesc()).thenReturn(chatMessage);
         mockMvc.perform(get(chatLink + "/last/message"))
             .andExpect(status().isOk());
 
-        verify(chatMessageService).findTopByOrderByIdDesc();
+        Mockito.verify(chatMessageService).findTopByOrderByIdDesc();
     }
 
     @Test
     void sentMessageTest() throws Exception {
         Long userId = 1L, roomId = 1L;
         String content = "content";
-        when(chatMessageService.sentMessage(eq(userId), eq(roomId), eq(content))).thenReturn(ChatMessageDto.builder()
+        Mockito.when(chatMessageService.sentMessage(Mockito.eq(userId), Mockito.eq(roomId), Mockito.eq(content))).thenReturn(ChatMessageDto.builder()
             .id(1L).build());
         mockMvc.perform(post(chatLink + "/sent-message/{userId}/{roomId}", userId, roomId)
             .param("content", content)).andExpect(status().isCreated());
 
-        verify(chatMessageService).sentMessage(userId, roomId, content);
+        Mockito.verify(chatMessageService).sentMessage(userId, roomId, content);
     }
 
     @Test
@@ -211,38 +219,38 @@ class ChatControllerTest {
         ChatRoomDto chatRoomDto = ChatRoomDto.builder().name("testName").build();
         ObjectMapper objectMapper = new ObjectMapper();
 
-        when(chatRoomService.createNewChatRoom(eq(chatRoomCreateDto))).thenReturn(chatRoomDto);
+        Mockito.when(chatRoomService.createNewChatRoom(Mockito.eq(chatRoomCreateDto))).thenReturn(chatRoomDto);
         mockMvc.perform(post(chatLink + "/create-chatRoom")
             .contentType(MediaType.APPLICATION_JSON)
             .content(objectMapper.writeValueAsString(chatRoomCreateDto))).andExpect(status().isCreated()).andReturn();
 
-        verify(chatRoomService).createNewChatRoom(eq(chatRoomCreateDto));
+        Mockito.verify(chatRoomService).createNewChatRoom(Mockito.eq(chatRoomCreateDto));
     }
 
     @Test
     void chatExistTest() throws Exception {
         Long fistUserId = 1L, secondUserId = 2L;
-        when(chatMessageService.chatExist(eq(fistUserId), eq(secondUserId))).thenReturn(
+        Mockito.when(chatMessageService.chatExist(Mockito.eq(fistUserId), Mockito.eq(secondUserId))).thenReturn(
             FriendsChatDto.builder()
                 .chatId(1L).build());
         mockMvc.perform(get(chatLink + "/exist/{fistUserId}/{secondUserId}", fistUserId, secondUserId))
             .andExpect(status().isOk());
 
-        verify(chatMessageService).chatExist(fistUserId, secondUserId);
+        Mockito.verify(chatMessageService).chatExist(fistUserId, secondUserId);
     }
 
     @Test
     void getGroupChatsTest() throws Exception {
         ChatType chatType = ChatType.GROUP;
-        when(principal.getName()).thenReturn("testmail@gmail.com");
+        Mockito.when(principal.getName()).thenReturn("testmail@gmail.com");
         Participant participant = new Participant();
-        when(participantService.findByEmail("testmail@gmail.com")).thenReturn(participant);
+        Mockito.when(participantService.findByEmail("testmail@gmail.com")).thenReturn(participant);
         List<ChatRoomDto> listOfGroupChatRooms = new ArrayList<>();
-        when(chatRoomService.findGroupChatRooms(participant, chatType)).thenReturn(listOfGroupChatRooms);
+        Mockito.when(chatRoomService.findGroupChatRooms(participant, chatType)).thenReturn(listOfGroupChatRooms);
         mockMvc.perform(get(chatLink + "/groups").principal(principal))
             .andExpect(status().isOk());
 
-        verify(chatRoomService).findGroupChatRooms(participant, chatType);
+        Mockito.verify(chatRoomService).findGroupChatRooms(participant, chatType);
     }
 
     @Test
@@ -250,12 +258,12 @@ class ChatControllerTest {
         MockMultipartFile file =
             new MockMultipartFile("file", new byte[1]);
         ChatMessageDto chatMessageDto = new ChatMessageDto();
-        when(azureFileService.saveFile(file)).thenReturn(chatMessageDto);
+        Mockito.when(azureFileService.saveFile(file)).thenReturn(chatMessageDto);
         mockMvc.perform(multipart(chatLink + "/upload/file")
             .file(file))
             .andExpect(status().isOk());
 
-        verify(azureFileService).saveFile(file);
+        Mockito.verify(azureFileService).saveFile(file);
     }
 
     @Test
@@ -263,12 +271,12 @@ class ChatControllerTest {
         MockMultipartFile file =
             new MockMultipartFile("file", new byte[1]);
         ChatMessageDto chatMessageDto = new ChatMessageDto();
-        when(this.azureFileService.saveVoiceMessage(file)).thenReturn(chatMessageDto);
+        Mockito.when(this.azureFileService.saveVoiceMessage(file)).thenReturn(chatMessageDto);
         mockMvc.perform(multipart(chatLink + "/upload/voice")
             .file(file))
             .andExpect(status().isOk());
 
-        verify(this.azureFileService).saveVoiceMessage(file);
+        Mockito.verify(this.azureFileService).saveVoiceMessage(file);
 
     }
 
@@ -277,13 +285,13 @@ class ChatControllerTest {
         mockMvc.perform(delete(chatLink + "/delete/file/{fileName}", "fileName"))
             .andExpect(status().isOk());
 
-        verify(this.azureFileService).deleteFile("fileName");
+        Mockito.verify(this.azureFileService).deleteFile("fileName");
     }
 
     @Test
     void addUserToSystemChatRoomTest() throws Exception {
         Long userId = 1L;
-        when(chatRoomService.addNewUserToSystemChat(userId)).thenReturn(userId);
+        Mockito.when(chatRoomService.addNewUserToSystemChat(userId)).thenReturn(userId);
 
         ObjectMapper objectMapper = new ObjectMapper();
 
@@ -292,7 +300,7 @@ class ChatControllerTest {
             .content(objectMapper.writeValueAsString(userId)))
             .andExpect(status().isOk());
 
-        verify(chatRoomService).addNewUserToSystemChat(userId);
+        Mockito.verify(chatRoomService).addNewUserToSystemChat(userId);
     }
 
     @Test
@@ -306,27 +314,27 @@ class ChatControllerTest {
     void processMessageTest() {
         ChatMessageDto chatMessageDto = ChatMessageDto.builder().id(1L).build();
         chatController.processMessage(chatMessageDto);
-        verify(chatMessageService).processMessage(chatMessageDto);
+        Mockito.verify(chatMessageService).processMessage(chatMessageDto);
     }
 
     @Test
     void deleteMessageTest() {
         ChatMessageDto chatMessageDto = ChatMessageDto.builder().id(1L).build();
         chatController.deleteMessage(chatMessageDto);
-        verify(chatMessageService).deleteMessage(chatMessageDto);
+        Mockito.verify(chatMessageService).deleteMessage(chatMessageDto);
     }
 
     @Test
     void updateMessageTest() {
         ChatMessageDto chatMessageDto = ChatMessageDto.builder().id(1L).build();
         chatController.updateMessage(chatMessageDto);
-        verify(chatMessageService).updateMessage(chatMessageDto);
+        Mockito.verify(chatMessageService).updateMessage(chatMessageDto);
     }
 
     @Test
     void likeMessageTest() {
         MessageLike messageLike = new MessageLike(1L, 1L);
         chatController.likeMessage(messageLike);
-        verify(chatMessageService).likeMessage(messageLike);
+        Mockito.verify(chatMessageService).likeMessage(messageLike);
     }
 }
