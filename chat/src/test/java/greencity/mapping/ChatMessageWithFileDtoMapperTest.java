acfc4139dd2e6e4db12b87d51eac3ab3d1d11191
Maryna <m51571750@gmail.com>
2024-07-01 15:21:19 +0300

    Added tests for ChatMessageServiceImplTest and ChatMessageWithFileDtoMapperTest

--- chat/src/test/java/greencity/mapping/ChatMessageWithFileDtoMapperTest.java
+++ chat/src/test/java/greencity/mapping/ChatMessageWithFileDtoMapperTest.java
@@ -1,6 +1,7 @@
 package greencity.mapping;
 
 import greencity.dto.ChatMessageWithFileDto;
+import greencity.dto.UserDto;
 import greencity.entity.ChatMessage;
 import greencity.entity.ChatRoom;
 import greencity.entity.Participant;
@@ -9,6 +10,10 @@ import greencity.enums.Role;
 import greencity.enums.UserStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -31,6 +36,41 @@ class ChatMessageWithFileDtoMapperTest {
 
     @Test
     void convert() {
+        assertEquals(expected, chatMessageWithFileDtoMapper.convert(chatMessage));
+    }
+
+    @Test
+    void convertWithLikes() {
+        Set<Participant> likes = new HashSet<>();
+        likes.add(new Participant(2L, "user2", "user2@example.com", null,
+                null, UserStatus.ACTIVATED, Role.ROLE_USER, null));
+        chatMessage.setLikes(likes);
+
+        Set<UserDto> expectedLikes = new HashSet<>();
+        expectedLikes.add(new UserDto(2L, "user2", null));
+        expected.setLikes(expectedLikes);
+
+        assertEquals(expected, chatMessageWithFileDtoMapper.convert(chatMessage));
+    }
+
+    @Test
+    void convertWithEmptyLikes() {
+        chatMessage.setLikes(Collections.emptySet());
+        expected.setLikes(Collections.emptySet());
+
+        assertEquals(expected, chatMessageWithFileDtoMapper.convert(chatMessage));
+    }
+
+    @Test
+    void convertWithNullFields() {
+        chatMessage.setFileName(null);
+        chatMessage.setFileType(null);
+        chatMessage.setFileUrl(null);
+
+        expected.setFileName(null);
+        expected.setFileType(null);
+        expected.setFileUrl(null);
+
         assertEquals(expected, chatMessageWithFileDtoMapper.convert(chatMessage));
     }
 }
--- chat/src/test/java/greencity/service/impl/ChatMessageServiceImplTest.java
+++ chat/src/test/java/greencity/service/impl/ChatMessageServiceImplTest.java
@@ -209,7 +209,17 @@ class ChatMessageServiceImplTest {
         chatMessageServiceImpl.likeMessage(messageLike);
         verify(chatMessageRepo).addLikeToMessage(1L, 1L);
     }
+    @Test
+    void likeMessageNotFound() {
+        MessageLike messageLike = new MessageLike(1L, 1L);
 
+        when(chatMessageRepo.getParticipantIdIfLiked(1L, 1L)).thenReturn(null);
+        doNothing().when(chatMessageRepo).addLikeToMessage(1L, 1L);
+        when(chatMessageRepo.findById(1L)).thenReturn(Optional.empty());
+
+        assertThrows(NotFoundException.class, () -> chatMessageServiceImpl.likeMessage(messageLike));
+        verify(chatMessageRepo, never()).deleteLikeFromMessage(anyLong(), anyLong());
+    }
     @Test
     void sendVoiceMessageTest() {
         ChatMessageDto inputDto = chatMessageDto;
