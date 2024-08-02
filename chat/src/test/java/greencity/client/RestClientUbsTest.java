1a4252026785eb077d44762df84ee8c13ee422a6
Maryna <m51571750@gmail.com>
2024-08-02 19:58:56 +0300

    Added RestClientUbsTest

---
+++ chat/src/test/java/greencity/client/RestClientUbsTest.java
@@ -0,0 +1,91 @@
+package greencity.client;
+
+import java.util.List;
+import greencity.dto.EmployeeWithTariffsDto;
+import greencity.dto.LocationsDto;
+import jakarta.servlet.http.HttpServletRequest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class RestClientUbsTest {
+
+    @InjectMocks
+    private RestClientUbs restClientUbs;
+
+    @Mock
+    private RestTemplate restTemplate;
+
+    @Mock
+    private HttpServletRequest httpServletRequest;
+
+    @Value("http://localhost:8080")
+    private String greenCityUbsServerAddress;
+
+    @Value("Bearer token")
+    private String accessToken;
+    private final HttpHeaders headers = new HttpHeaders();
+
+    @Test
+    void checkIfTariffExistsByIdTest() {
+        Long tariffInfoId = 1L;
+        String url = greenCityUbsServerAddress + "/ubs/check-if-tariff-exists/" + tariffInfoId;
+        headers.set("Authorization", accessToken);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        when(httpServletRequest.getHeader("Authorization")).thenReturn(accessToken);
+        when(restTemplate.exchange(url, HttpMethod.GET, entity, Boolean.class)).thenReturn(ResponseEntity.ok(true));
+
+        assertTrue(restClientUbs.checkIfTariffExistsById(tariffInfoId));
+    }
+
+    @Test
+    void getAllLocationsByCourierIdTest() {
+        Long courierId = 1L;
+        String url = greenCityUbsServerAddress + "/ubs/locationsByCourier/" + courierId;
+        headers.set("Authorization", accessToken);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        List<LocationsDto> locations = List.of(new LocationsDto());
+
+        when(httpServletRequest.getHeader("Authorization")).thenReturn(accessToken);
+        when(restTemplate.exchange(url, HttpMethod.GET, entity, new ParameterizedTypeReference<List<LocationsDto>>() {
+        }))
+            .thenReturn(ResponseEntity.ok(locations));
+
+        List<LocationsDto> result = restClientUbs.getAllLocationsByCourierId(courierId);
+
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    void getEmployeeByEmailTest() {
+        String email = "test@example.com";
+        String url = greenCityUbsServerAddress + "/admin/ubs-employee/" + email;
+        headers.set("Authorization", accessToken);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        EmployeeWithTariffsDto employee = new EmployeeWithTariffsDto();
+
+        when(httpServletRequest.getHeader("Authorization")).thenReturn(accessToken);
+        when(restTemplate.exchange(url, HttpMethod.GET, entity,
+            new ParameterizedTypeReference<EmployeeWithTariffsDto>() {
+            }))
+                .thenReturn(ResponseEntity.ok(employee));
+
+        EmployeeWithTariffsDto result = restClientUbs.getEmployeeByEmail(email);
+
+        assertNotNull(result);
+    }
+}
--- chat/src/test/java/greencity/service/client/RestClientUserTest.java
+++ chat/src/test/java/greencity/service/client/RestClientUserTest.java
@@ -22,7 +22,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class RestClientTest {
+public class RestClientUserTest {
     @InjectMocks
     private RestClientUser restClientUser;
     @Mock
